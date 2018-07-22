d0fcbd02b468cf84fbbe600c2ddd4864f49614c4
blueskyxzy <blueskyxzy@gmail.com>
2018-07-22 22:47:08 +0800

    测试静态代码块

---
+++ src/StaticTest.java
@@ -0,0 +1,17 @@
+import com.xzy.Test.TestStatic;
+
+/**
+ * Created by xzy on 18/7/17  .
+ */
+
+public class StaticTest {
+
+    public static void main(String[] args){
+        Long x = TestStatic.upload();
+        System.out.println(x);
+
+        Long upload = TestStatic.upload();
+
+
+    }
+}
---
+++ src/com/xzy/Test/TestStatic.java
@@ -0,0 +1,24 @@
+package com.xzy.Test;
+
+/**
+ * Created by xzy on 18/7/17  .
+ */
+
+// 测试静态代码块
+
+public class TestStatic {
+    private static Long x;
+    static {
+        try{
+            x = 3L;
+        }catch (Exception e) {
+
+        }
+    }
+
+    public static Long upload(){
+        System.out.println(x);
+        return x;
+    }
+}
+
