4c528e97df495e0d81f9d28cf9016d31c382762f
blueskyxzy <blueskyxzy@gmail.com>
2018-08-21 17:31:50 +0800

    浅拷贝测试

---
+++ src/testClone/Client.java
@@ -0,0 +1,21 @@
+package testClone;
+
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * Created by xzy on 18/8/21  .
+ */
+
+public class Client {
+    public static void main(String[] args){
+        User userA = new User("张三", 15L);
+        User userB = new User("王五", 27L);
+        User userC = userA;
+        userA = userB;       // 对象赋值是浅拷贝，地址相同
+        userA.setAge(16L);
+        userA.setAge(17L);
+        System.out.println(userA.toString());
+        System.out.println(userB.toString());
+        System.out.println(userC.toString());
+    }
+}
