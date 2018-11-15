d3b2cc69970a5dd805b0366ca90c5ce3cf9e8e3a
blueskyxzy <blueskyxzy@gmail.com>
2018-11-15 14:40:34 +0800

    hashMap test

--- src/com/xzy/test/testHashMap/TestHashMapClient.java
+++ src/com/xzy/test/testHashMap/TestHashMapClient.java
@@ -2,6 +2,7 @@ package com.xzy.test.testHashMap;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class TestHashMapClient {
 
@@ -10,18 +11,27 @@ public class TestHashMapClient {
         map.put(1, "11111111");
         System.out.println(map.get(1));
 
-        Map<Integer, String> myMap = new MyHashMap<>();
+//        Map<Integer, String> myMap = new MyHashMap<>();
 //        for(int i = 0; i<=20; i++){
 //            myMap.put(i, "1111111111111111");
 //        }
-//        Map<String, String> stringMap = new MyHashMap<>();
-//        for(int i = 0;i <= 20; i++){
-//            stringMap.put(String.valueOf(i), "2222222222222");
-//        }
-        for (int i =0; i <= 20; i++){
-            myMap.put(1, String.valueOf(i));
+        Map<String, String> stringMap = new MyHashMap<>();
+        for(int i = 0;i <= 20; i++){
+            stringMap.put(String.valueOf(i), "2222222222222");
         }
-        System.out.println(myMap.get(1));
+//        for (int i =0; i <= 20; i++){
+//            myMap.put(1, String.valueOf(i));
+//        }
+//        System.out.println(myMap.get(1));
+
+        // 测试迭代
+        Set<Integer> keys = map.keySet();
+
+        for(Map.Entry<Integer, String> entity: map.entrySet()){
+            Integer key = entity.getKey();
+            String value = entity.getValue();
+            System.out.println("key:" + key + ", value:" + value);
+        }
     }
 
 }
