8c6779877c444df73080417703bc9aec0e69473d
blueskyxzy <blueskyxzy@gmail.com>
2018-12-11 15:09:03 +0800

    test work

---
+++ src/com/xzy/test/work/TestClient.java
@@ -0,0 +1,10 @@
+package com.xzy.test.work;
+
+public class TestClient {
+
+    public static void main(String[] args){
+        String s = "http://app-common.oss-cn-hangzhou.aliyuncs.com/changru/gwgx/promotion/1544510536858.jpg?Expires=1859870537&OSSAccessKeyId=LTAI23gFjKV5y5Ew&Signature=BRbV8ISrS02YvftZcd5yA0CwDV0%3D";
+        System.out.println(s.length());
+    }
+
+}
