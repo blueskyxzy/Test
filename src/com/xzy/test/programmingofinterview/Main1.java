48e336be2012f3bd26dc9dc4cbd96986002a197c
blueskyxzy <blueskyxzy@gmail.com>
2019-01-07 16:17:12 +0800

    网易编程题1 优雅的点java实现和优化

--- src/com/xzy/test/programmingofinterview/Main1.java
+++ src/com/xzy/test/programmingofinterview/Main1.java
@@ -25,29 +25,78 @@ import java.util.Scanner;
 //        输出例子1:
 //        12
 
-public class Main1{
+public class Main1 {
 
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        String str = input.nextLine();
-        int x = Integer.parseInt(str);
-        long startTime = System.nanoTime();
-        int y = calcuatePoint(x);
-        System.out.println(y);
-        long endTime = System.nanoTime();
-        System.out.println("程序运行时间： "+(endTime - startTime)+"ns");
-    }
+    //    public static void main(String[] args){
+//        Scanner input = new Scanner(System.in);
+//        String str = input.nextLine();
+//        int x = Integer.parseInt(str);
+//        long startTime = System.nanoTime();
+//        int y = calcuatePoint(x);
+//        System.out.println(y);
+//        long endTime = System.nanoTime();
+//        System.out.println("程序运行时间： "+(endTime - startTime)+"ns");
+//    }
+//
+//    public static int calcuatePoint(int x){
+//        int a = (int) Math.sqrt(x);
+//        int count = 0;
+//        for (int i = -a; i <= a; i++){
+//            for (int j = -a; j <= a; j++){
+//                if (Math.pow(i, 2) + Math.pow(j, 2) == x){
+//                    count++;
+//                }
+//            }
+//        }
+//        return count;
+//    }
 
-    public static int calcuatePoint(int x){
-        int a = (int) Math.sqrt(x);
+
+//    解答一
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        System.out.println(count(n));
+//    }
+//
+//    public static int count(int n) {
+//        int count = 0;
+//        double r = Math.sqrt(n);
+//        for (int i = 0; i < r; i++) {
+//            double j = Math.sqrt(n - i * i);
+//            if (Math.abs(j - Math.round(j)) <= 0.000000001) {
+//                count++;
+//            }
+//        }
+//        return 4 * count;
+//    }
+
+//    解答2
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int rSquare = in.nextInt();
         int count = 0;
-        for (int i = -a; i <= a; i++){
-            for (int j = -a; j <= a; j++){
-                if (Math.pow(i, 2) + Math.pow(j, 2) == x){
+        double r = Math.sqrt(rSquare);
+
+        //存储值
+        for (int i = 0; i < r; i++) {
+            /*运行超时
+            for(int j=1;j<r+1;j++){
+                if(i*i+j*j==rSquare){
                     count++;
                 }
             }
+            */
+//优化点1
+            double j = Math.sqrt(rSquare - i * i);
+            if ((int) j == j) {
+                count++;
+            }
         }
-        return count;
+
+        //优化点2
+        System.out.print(count << 2);
+
     }
+
 }
